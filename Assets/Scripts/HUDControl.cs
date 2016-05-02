f00aacc473205951b0348cb9d55588bf0503e2e5
BarHaran <barharan222@gmail.com>
2016-05-02 14:06:45 +0300

    Multiplayer Update 1

---
+++ Assets/Scripts/HUDControl.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+
+[AddComponentMenu("Network/NetworkManagerHUD")]
+[System.ComponentModel.EditorBrowsable(System.ComponentModel.EditorBrowsableState.Never)]
+public class HUDControl : NetworkBehaviour {
+
+    public NetworkManagerHUD hudManager;
+
+    // Use this for initialization
+    void Start ()
+    {
+        hudManager.showGUI = false;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (isLocalPlayer)
+        {
+            if (Input.GetKey(KeyCode.P))
+            {
+                hudManager.showGUI = !hudManager.showGUI;
+            }
+        }
+	}
+}
---
+++ Assets/Scripts/LocalPlayerSetup.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+using UnityStandardAssets.Characters.FirstPerson;
+
+public class LocalPlayerSetup : NetworkBehaviour
+{
+
+    public Camera playercamera;
+    public FirstPersonController player;
+    public GameObject SceneCam;
+    public Camera scene;
+    public AudioListener mainaudio;
+
+    public NetworkManager network;
+
+    // Use this for initialization
+    void Start()
+    {
+        if (isLocalPlayer)
+        {
+            player.enabled = true;
+            playercamera.enabled = true;
+            SceneCam = GameObject.Find("SceneCam");
+            scene = SceneCam.GetComponent<Camera>();
+            scene.enabled = false;
+            mainaudio = SceneCam.GetComponent<AudioListener>();
+            mainaudio.enabled = false;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (string.IsNullOrEmpty(network.matchName))
+        {
+            scene.enabled = true;
+        }
+    }
+}
