9466dab006feab3f9bf11636c3711426a0b97201
ororor2000 <ororor2000@gmail.com>
2016-05-04 13:39:37 +0300

    shit

--- Assets/Scripts/LocalPlayerSetup.cs
+++ Assets/Scripts/LocalPlayerSetup.cs
@@ -5,39 +5,67 @@ using UnityStandardAssets.Characters.FirstPerson;
 
 public class LocalPlayerSetup : NetworkBehaviour
 {
+    //declering required varibles
+    //scene camera and its audio listener
+    public Camera sceneCamera;
+    public AudioListener sceneCamerAudioListener;
 
-    public Camera playercamera;
-    public FirstPersonController player;
-    public GameObject SceneCam;
-    public Camera scene;
-    public AudioListener mainaudio;
+    //the player his attached camera and the controller script of the player
+    public GameObject player;
+    public Camera playerCamera;
+    public FirstPersonController playerController;
 
-    public NetworkManager network;
+    //the network Manager
+    public NetworkManager networkManager;
+    public NetworkConnection client;
 
-    // Use this for initialization
     void Start()
     {
+        //getting the varibes 
+        sceneCamera = GameObject.Find("SceneCam").GetComponent<Camera>();
+        sceneCamerAudioListener = GameObject.Find("SceneCam").GetComponent<AudioListener>();
+
+        player = this.gameObject;
+        playerCamera = player.GetComponentInChildren<Camera>();
+        playerController = player.GetComponent<FirstPersonController>();
+
+        networkManager = GameObject.Find("NetworkManager").GetComponent<NetworkManager>();
+        client = new NetworkConnection();
+
+        //diabling and enabling the varibles
         if (isLocalPlayer)
         {
-            player.enabled = true;
-            playercamera.enabled = true;
-            SceneCam = GameObject.Find("SceneCam");
-            scene = SceneCam.GetComponent<Camera>();
-            scene.enabled = false;
-            mainaudio = SceneCam.GetComponent<AudioListener>();
-            mainaudio.enabled = false;
+            //disabling the scene Camera and its audio Listner to avoid collisns betwwen the player and the scene Camera
+            ChangeSceneCameraState(false);
+
+            //enabling the player and its controller script
+            ChangePlayerState(true);
         }
     }
 
-    // Update is called once per frame
     void Update()
+    {       
+    }
+
+    void OnGUI()
     {
-        if (Input.GetKey(KeyCode.X))
+        if (Input.GetKey(KeyCode.Insert))
         {
-            scene = SceneCam.GetComponent<Camera>();
-            scene.enabled = true;
-            mainaudio = SceneCam.GetComponent<AudioListener>();
-            mainaudio.enabled = true;
+            Debug.Log("clicked");
         }
     }
+
+    public void ChangeSceneCameraState(bool state)
+    {
+        sceneCamera.enabled = state;
+        sceneCamerAudioListener.enabled = state;
+    }
+
+    public void ChangePlayerState(bool state)
+    {
+        player.SetActive(state);
+        playerCamera.enabled = state;
+        playerController.enabled = state;
+    }    
 }
+
